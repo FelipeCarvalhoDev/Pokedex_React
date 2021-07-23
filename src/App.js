291cc4e8a811d869a20b2f11f7902234b48b3b60
Felipe Carvalho <felipelima.web@gmail.com>
2021-07-22 22:00:52 -0300

    Criação dos cards

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import './assets/scss/App.scss';
 import Header from './components/Header';
 import Filter from './components/Filter';
 import Card from './components/Card';
@@ -6,30 +7,40 @@ import Card from './components/Card';
 const api = 'https://pokeapi.co/api/v2/pokemon/'
 
 function App(){
-const [text, setText] = useState('');
+const [name_id, setNameId] = useState('');
 const [info, setInfo] = useState({});
 
 useEffect(() => {
-  if (text) {
-    fetch(`${api}${text}`)
+  if (name_id) {
+    fetch(`${api}${name_id}`)
       .then((response) => response.json())
       .then((response) => {
+        console.log(response)
         setInfo(response)
       })
   } else {
     fetch(`${api}`)
       .then((response) => response.json())
       .then((response) => {
-        setInfo(response)
+          setInfo(response)
       })
   }
-}, [text]);
+}, [name_id]);
 
 return (
     <div className="app">
       <Header />
-      <Filter value={text} onChange={(search) => setText(search)}/>
-      <Card data={info} />
+      <Filter value={name_id} onChange={(search) => setNameId(search)}/>
+      <div className="results">
+        {info.results &&
+          info.results.map((pokemon) => (
+            <Card key={pokemon.url} data={pokemon}/>
+        ))}
+        {info.forms && 
+          info.forms.map((pokemon) => (
+            <Card key={pokemon.url} data={pokemon.name}/>
+        ))}      
+      </div>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,28 +1,54 @@
 
 import './../assets/scss/Card.scss';
+import { useEffect, useState } from 'react';
+import Divider from '@material-ui/core/Divider';
+
+const api = 'https://pokeapi.co/api/v2/pokemon/'
 
 function Card({data}) {
-  
+const [infoPoke, setInfo] = useState({});
+
+useEffect(() => {  
+  if(data.url){
+    fetch(`${data.url}`)
+      .then((response) => response.json())
+      .then((response) => {
+        setInfo(response)
+      })
+  } else {
+    fetch(`${api}${data}`)
+      .then((response) => response.json())
+      .then((response) => {
+        setInfo(response)
+      })
+  }
+}, []);
+
   return (
     <div className="card">
-        {data.results && (
-        <ul>
-          {data.results.map((pokemon) => (
-            <li key={pokemon.url}>
-              {pokemon.name}
-            </li>
-          ))}
-        </ul>
-      )}
-      {data.forms && (
-        <ul>
-          {data.forms.map((pokemon) => (
-            <li key={pokemon.url}>
-              {pokemon.name}
-            </li>
-          ))}
-        </ul>
-      )}
+      
+      <img src={infoPoke.sprites?.front_default} alt="" />
+      <p className="name">{infoPoke.id} - {data.name ? data.name : data}</p>
+      <Divider />
+      <p>
+        <b>Tipo:</b><br />
+      <span>|</span>
+      {infoPoke.types && 
+        infoPoke.types.map((type) => (
+          <span key={type.slot}> {type.type.name} |</span>
+        ))
+      }
+      </p>
+      <Divider />
+      <p>
+        <b>Habilidades:</b><br />
+      <span>|</span>
+      {infoPoke.abilities && 
+        infoPoke.abilities.map((ability) => (
+          <span key={ability.slot}> {ability.ability.name} |</span>
+        ))
+      }
+      </p>
     </div>
   );
 }
